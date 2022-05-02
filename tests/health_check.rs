a38ec2b67f9c1c851fb2434873fc865ef6e320d7
Ueki Masaru <masaru.ueki@itage.co.jp>
2022-05-02 14:13:06 +0900

    fix unittest

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -47,7 +47,7 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -137,3 +137,31 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fileds_are_present_but_empty() {
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return a 200 OK when the payload was {}.",
+            description
+        );
+    }
+}
